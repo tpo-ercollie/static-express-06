cebc88127da7ff9c2312811a4bc6e213bef30e21
Erica Collier <ercollie@travelers.com>
2021-02-19 10:24:34 -0500

    commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const { projects } = require('..data.json')
+// Import Express and set up the app
+const express = require('express');
+const app = express();
 
- /* Home Page */
-router.get('/', function(req, res, next) {
-    // passing project info to the 'index' pug
-    res.render('index'. { projects });
-});
+// Create variable to link the data.json file
+const { projects } = require('../static-express-06/data.json');
+
+
+//Set up middleware
+
+app.use((req, res, next) = {})
+// Import Routes
+
+// Import 404 and global error handlers
+
+// Pass route handlers to the app
+
+// Pass 404 and global error handlers to the app
+
+// Activate the port
+app.listen(3000);
+
 
